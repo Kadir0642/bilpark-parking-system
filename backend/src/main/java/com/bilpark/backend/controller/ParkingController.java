345f5e7e772d124cdab450d1c588f1b79f104463
Kadir0642 <kadirk061942@gmail.com>
2026-02-14 19:24:45 +0300

    feat: Implement Check-in, Check-out and Fee calculation logic

--- backend/src/main/java/com/bilpark/backend/controller/ParkingController.java
+++ backend/src/main/java/com/bilpark/backend/controller/ParkingController.java
@@ -2,10 +2,13 @@ package com.bilpark.backend.controller;
 
 // Adres Tarifleri
 import com.bilpark.backend.model.ParkSpot;
+import com.bilpark.backend.model.ParkingRecord;
 import com.bilpark.backend.service.ParkingService;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController //Bu sınıf artık REST API (sistemlere JSON cevap verir)
@@ -20,12 +23,29 @@ public class ParkingController
         this.parkingService=parkingService; // istekleri iletebilmemiz için
     }
 
-    //"GET" (Getir/Oku) isteği: http://localhost:8080/api/parking/spots
-    //Tüm park yerlerini(spots) JSON olarak döner  |  controller -> service -> repository -> service -> controller ->Müşteri(JSON olarak)
+    //"GET" (Getir/Oku) isteği: URL: http://localhost:8080/api/parking/spots
+    //Tüm park yerlerini(spots) JSON listesi olarak döner  |  controller -> service -> repository -> service -> controller ->Müşteri(JSON olarak)
     @GetMapping("/spots")
     public List<ParkSpot> getAllSpots()
     {
         return parkingService.getAllSpots();
     }
-    //PostMapping olsaydı "Yeni park yeri ekle" denirdi.
+
+    //1. CHECK-IN (Giris Yap) | Eylem varsa POST kullanılır.
+    // URL: http://localhost:8080/api/parking/check-in?spotId=1&licensePlate=34ABC123
+    //@RequestParam: URL'in sonundaki soru işaretinden sonraki verileri okur.
+    @PostMapping("/check-in") // GİRİŞ İŞLEMLERİ | PostMapping (Postala/Gönder) "Yeni park yeri eklerken" Sunucuda değişiklik, oluşturma, silme gibi işleri vardır(CRUD)
+    public ParkSpot checkIn(@RequestParam Long spotId,@RequestParam String licensePlate)
+    {
+        return parkingService.checkInVehicle(spotId,licensePlate); // Controller, gelen verileri servise verir. | Sonucta -> Araci iceri alir ve park yerinin son halini basar.
+    }
+
+    //2. CHECK-OUT (Cikis Yap)
+    //URL: http://localhost:8080/api/parking/check-out?spotId=1
+    @PostMapping("/check-out")
+    public ParkingRecord checkOut(@RequestParam Long spotId) // Sadece ID bilgisini service
+    {
+        return parkingService.checkOutVehicle(spotId); // Arac cikisini ve ücret hesabini yapar. | Sonucta -> Fis/Fatura ücret bilgisi gösterilir.
+    }
+
 }
