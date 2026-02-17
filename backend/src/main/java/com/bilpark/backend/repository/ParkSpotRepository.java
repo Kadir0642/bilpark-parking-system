e4b978de3f2e27a1874b9652060405b0e6bd6e3a
Kadir0642 <kadirk061942@gmail.com>
2026-02-17 12:30:31 +0300

    feat: Database sorguları (sokak/mahalle/Bölge) eklendi

--- backend/src/main/java/com/bilpark/backend/repository/ParkSpotRepository.java
+++ backend/src/main/java/com/bilpark/backend/repository/ParkSpotRepository.java
@@ -4,6 +4,8 @@ import com.bilpark.backend.model.ParkSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository // Spring'e : ParkSpotRepository-> veritabanı işlerinden sorumlu der. (Dependency Injection) veritabanı hatalarını bizim dilimize çeviren bir Tercüman
 public interface ParkSpotRepository extends JpaRepository<ParkSpot, Long>
 {
@@ -13,4 +15,9 @@ public interface ParkSpotRepository extends JpaRepository<ParkSpot, Long>
     * .delete()  -> Sil
     * .findById() -> ID ile bul
     */
+
+    //SELECT * FROM park_spots WHERE street = 'Atatürk Caddesi';
+    List<ParkSpot> findByStreet(String street); // Parametre de gelen sokak adını veritabanında arayıp park yerlerini liste olarak bana ver.
+    List<ParkSpot> findByNeighborhood(String neighborhood); // Mahalle ye göre filtre yaparak park alanlarını verir
+    List<ParkSpot> findByRegion(String region,String neighborhood); // Hem mahalle hem ilçe filtresi
 }
