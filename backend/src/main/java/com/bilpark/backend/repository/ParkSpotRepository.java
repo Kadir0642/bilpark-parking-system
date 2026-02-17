93b8856a1108bdcd1ed151fff914340d9a7c1658
Kadir0642 <kadirk061942@gmail.com>
2026-02-17 14:04:09 +0300

    refactor: Yorum satırları düzeltildi

--- backend/src/main/java/com/bilpark/backend/repository/ParkSpotRepository.java
+++ backend/src/main/java/com/bilpark/backend/repository/ParkSpotRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+//Park yeri filtreleri/büyüklüğü ile ilgilenir.
 @Repository // Spring'e : ParkSpotRepository-> veritabanı işlerinden sorumlu der. (Dependency Injection) veritabanı hatalarını bizim dilimize çeviren bir Tercüman
 public interface ParkSpotRepository extends JpaRepository<ParkSpot, Long>
 {
@@ -21,7 +22,7 @@ public interface ParkSpotRepository extends JpaRepository<ParkSpot, Long>
     List<ParkSpot> findByNeighborhood(String neighborhood); // Mahalle ye göre filtre yaparak park alanlarını verir
     List<ParkSpot> findByRegion(String neighborhood); //İlçe filtresi
 
-    List<ParkSpot> findByRegionAndNeigborhoodAndStreet(String region,String neighborhood,String street); // İlçe,Mahalle,sokak filtresi (İnce ayar)
+    List<ParkSpot> findByRegionAndNeighborhoodAndStreet(String region,String neighborhood,String street); // İlçe,Mahalle,sokak filtresi (İnce ayar)
     List<ParkSpot> findByRegionAndNeighborhood(String region,String neighborhood);// İlçe,mahalle filtresi (Daha geniş arama)
 
     List<ParkSpot> findByOccupiedTrue(); // Dolu park yerleri filtresi  BURAYI ZATEN IZGARA İLE GÖSTERECEĞİZ
