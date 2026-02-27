b9ff250e91dc909afd24fd6d9356fb12436e6e1a
Kadir0642 <kadirk061942@gmail.com>
2026-02-27 23:18:05 +0300

    fix: findByOccupiedTrue ve spotName metotları silindi (Tabloda olan bir araç zaten sokakta ve aktiftir)

--- backend/src/main/java/com/bilpark/backend/repository/ParkSpotRepository.java
+++ backend/src/main/java/com/bilpark/backend/repository/ParkSpotRepository.java
@@ -1,6 +1,8 @@
 package com.bilpark.backend.repository;
 
 import com.bilpark.backend.model.ParkSpot;
+import com.bilpark.backend.model.StreetLocation;
+import com.bilpark.backend.model.ParkingStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -18,17 +20,19 @@ public interface ParkSpotRepository extends JpaRepository<ParkSpot, Long>
     * .findById() -> ID ile bul
     */
 
-    //SELECT * FROM park_spots WHERE street = 'Atatürk Caddesi';
-    List<ParkSpot> findByStreet(String street); // Parametre de gelen sokak adını veritabanında arayıp park yerlerini liste olarak bana ver.
-    List<ParkSpot> findByNeighborhood(String neighborhood); // Mahalle ye göre filtre yaparak park alanlarını verir
-    List<ParkSpot> findByRegion(String neighborhood); //İlçe filtresi
-
-    List<ParkSpot> findByRegionAndNeighborhoodAndStreet(String region,String neighborhood,String street); // İlçe,Mahalle,sokak filtresi (İnce ayar)
-    List<ParkSpot> findByRegionAndNeighborhood(String region,String neighborhood);// İlçe,mahalle filtresi (Daha geniş arama)
-
+    // 1. Plakaya göre sokaktaki aracı bul (Çifte kayıt güvenliği ve Çıkış işlemi için)
     //Plakaya göre dolu park yerlerini  bul (büyük/küçük fark etmeksizin) | Optional -> (Null kontrolleri daha güvenli ve okunabilir olsun diye)Bir değerin mevcut olup olmadığını belirtmek için kullanılır.
     Optional<ParkSpot> findByCurrentPlateIgnoreCase(String currentPlate);
 
-    List<ParkSpot> findByOccupiedTrue(); // Dolu park yerleri filtresi  BURAYI ZATEN IZGARA İLE GÖSTERECEĞİZ
+    // 2. Sadece belirli bir caddedeki aktif araçları getir. (Enum kullanıyoruz !)
+    //SELECT * FROM park_spots WHERE street = 'Atatürk Caddesi';
+    List<ParkSpot> findByStreet(StreetLocation street); // Parametre de gelen sokak adını veritabanında arayıp park yerlerini liste olarak bana ver.
+
+    // 3. Cadde, Mahalle ve İlçe filtreli arama
+    List<ParkSpot> findByRegionAndNeighborhoodAndStreet(String region, String neighborhood, StreetLocation street);
+    List<ParkSpot> findByRegionAndNeighborhood(String region, String neighborhood);// İlçe,mahalle filtresi (Daha geniş arama)
+
+    // 4. Belirli bir durumda olan araçları getir. (Örn: Sadece ACTIVE olanlar)
+    List<ParkSpot> findByStatus(ParkingStatus status);
 
 }
