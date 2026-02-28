531cee33944f38d40462e020ebdc27cf22e1332b
Kadir0642 <kadirk061942@gmail.com>
2026-02-28 15:32:25 +0300

    fix: DataInitializer sag-sol parametre hatasi giderildi

--- backend/src/main/java/com/bilpark/backend/config/DataInitializer.java
+++ backend/src/main/java/com/bilpark/backend/config/DataInitializer.java
@@ -8,7 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//BİZ EKLEDİKÇE PARK YERİ GELSE NASIL OLUR - SİSTEM AÇILIR AÇILMAZ PARK YERİ OLUŞTURMASIN -ZATEN PARK YERİ KAPASİTESİ BELLİ
+// BİZ EKLEDİKÇE PARK YERİ GELSE NASIL OLUR - SİSTEM AÇILIR AÇILMAZ PARK YERİ OLUŞTURMASIN -ZATEN PARK YERİ KAPASİTESİ BELLİ
 
 @Configuration // Ayar sınıfı olduğunu söyler | "Seed Data" (Tohum Verisi) yüklemek
 public class DataInitializer
