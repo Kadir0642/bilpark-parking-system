a861ff92b4c1764f8e645d8872cf018fe7db6200
Kadir0642 <kadirk061942@gmail.com>
2026-03-02 15:41:45 +0300

    refactor: data seeder temizlik

--- backend/src/main/java/com/bilpark/backend/config/DataInitializer.java
+++ backend/src/main/java/com/bilpark/backend/config/DataInitializer.java
@@ -11,25 +11,4 @@ import org.springframework.context.annotation.Configuration;
 // BİZ EKLEDİKÇE PARK YERİ GELSE NASIL OLUR - SİSTEM AÇILIR AÇILMAZ PARK YERİ OLUŞTURMASIN -ZATEN PARK YERİ KAPASİTESİ BELLİ
 
 @Configuration // Ayar sınıfı olduğunu söyler | "Seed Data" (Tohum Verisi) yüklemek
-public class DataInitializer
-{
-    // CommandLineRunner: Uygulama başlar başlamaz çalışacak olan metod.
-    @Bean 
-    CommandLineRunner initDataBase(ParkSpotRepository repository) {
-        return args -> {
-            // Eğer veritabanu tamamen boşsa (0 araç varsa), test için 3 araç ekleriz.
-            if(repository.count()==0){
-                // Tevfik Bey caddesine küçük bir araç
-                repository.save(new ParkSpot("34TEST01",StreetLocation.TEVFIK_BEY, VehicleType.SMALL, "Merkez", "Bilecik","LEFT"));
-
-                // Cumhuriyet caddesine büyük bir ticari araç
-                repository.save(new ParkSpot("06BASKAN99", StreetLocation.CUMHURIYET, VehicleType.LARGE, "Merkez", "Bilecik","RIGHT"));
-
-                // Ali Rıza Özkay caddesine senin adınla bir araç :)
-                repository.save(new ParkSpot("11KADIR01", StreetLocation.ALI_RIZA_OZKAY, VehicleType.SMALL, "Merkez", "Bilecik","LEFT"));
-
-                System.out.println("✅ BİLGİ: BilPark 2.0 Dinamik Test Araçları sokağa park edildi!");
-            }
-        };
-    }
-}
+public class DataInitializer{}
